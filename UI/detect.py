2f2b5a12addff276adbb2052a55215b19d6be137
Biparnak Roy <roybiparnak@gmail.com>
2020-10-15 13:29:46 +0530

    adding detection engine

--- UI/detect.py
+++ UI/detect.py
@@ -1,7 +1,10 @@
-def detect_it(text1,text2,text3):
-        print(text1)
-        print(text2)
-        print(text3)
+import subprocess
+def detect_it(CAM_ID,CAM_IP,STREAM_PORT):
+        #print(CAM_ID)
+        #print(CAP_IP)
+        #print(STREAM_PORT)
+        darknet_detect="cd ../darknet && ./darknet detector demo data/obj.data yolo-obj-test.cfg backup/yolo-obj_best.weights " + CAM_IP
+        subprocess.run(darknet_detect, shell=True)
 
 if __name__ == '__main__':
     # test1.py executed as script
