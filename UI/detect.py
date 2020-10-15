58297e2c3e8c2f22ba5a5b387b7ac48217660e28
Biparnak Roy <roybiparnak@gmail.com>
2020-10-15 13:46:55 +0530

    fixed detection engine path

--- UI/detect.py
+++ UI/detect.py
@@ -3,7 +3,7 @@ def detect_it(CAM_ID,CAM_IP,STREAM_PORT):
         #print(CAM_ID)
         #print(CAP_IP)
         #print(STREAM_PORT)
-        darknet_detect="cd ../darknet && ./darknet detector demo data/obj.data yolo-obj-test.cfg backup/yolo-obj_best.weights " + CAM_IP
+        darknet_detect="cd ../darknet_detection && ./darknet detector demo data/obj.data yolo-obj-test.cfg backup/yolo-obj_best.weights " + CAM_IP
         subprocess.run(darknet_detect, shell=True)
 
 if __name__ == '__main__':
